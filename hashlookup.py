5d8f8ccca34092805515709c6128b915215c45f4
Hnav3 <eaholznecht@gmail.com>
2022-04-14 11:59:31 -0500

    Updating config file

--- hashlookup.py
+++ hashlookup.py
@@ -1,16 +1,16 @@
 import vt
-import os, sys, csv, argparse
+import os, sys, csv, argparse, configparser
 from ratelimit import limits, sleep_and_retry
 
 script_path = os.path.dirname(os.path.realpath(__file__))
-
-AV_ENGINES = ['AV1','AV2','AV3']
-CSV_FILE=f'{script_path}/lookup.csv'
-CSV_OBJ=[]
+config=configparser.ConfigParser()
+config.read(f'{script_path}/config.ini')
 
 ## Rate Limiting. Public API only allows 4 lookups/min
-CALLS = 4
-RATE_LIMIT=60
+CALLS = config.getint('API','API_CALLS')
+RATE_LIMIT=config.getint('API','API_RATE')
+
+CSV_OBJ=[]
 
 def open_vt_session(apikey):
     client = vt.Client(apikey)
@@ -24,7 +24,7 @@ def close_vt_session(client):
 def check_limit():
     return
 
-def get_vt_file_obj(client,hash):
+def get_vt_file_obj(client,hash,AV_ENGINES):
     try:
         file = client.get_object(f'/files/{hash}')
         print(f"Getting info for {hash}")
@@ -36,40 +36,46 @@ def get_vt_file_obj(client,hash):
         file_obj = {'hash':f'{hash} NOT FOUND'}
         CSV_OBJ.append(file_obj)
 
-def csv_writer():
+def csv_writer(filename):
     csv_fields = ['hash','file_type','av_engine','av_category','av_signature']
 
-    with open(CSV_FILE, 'w') as csvfile:
+    with open(filename, 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=csv_fields)
         writer.writeheader()
         writer.writerows(CSV_OBJ)
 
-def main():
+def arg_parse():
     parser = argparse.ArgumentParser("VirusTotal hash lookup tool.")
-    parser.add_argument("--apikey", required=True, help="API Key from VirusTotal.com.")
-    parser.add_argument("--infile", help="CSV File of hashes to search.")
-    parser.add_argument("--hash", help="Hash you'd like to search.")
-    args = parser.parse_args()
+    parser.add_argument("-i", "--infile", help="CSV File of hashes to search.")
+    parser.add_argument("-v", "--value", type=str, help="Hash you'd like to search.")
+    parser.add_argument("-s", "--save", type=str, help="Save the CSV file with a different filename.")
+    return parser
 
-    if not args.infile and not args.hash:
+def main(args):
+    if not args.infile and not args.value:
         print("Input file or individual hash is required to run this tool")
         sys.exit()
-    elif args.infile and args.hash:
+    elif args.infile and args.value:
         print("Input file and individual hash were provided. Please choose one.")
         sys.exit()
     else:
-        client = open_vt_session(args.apikey)
-        if args.hash:
+        filename = args.save or "lookupresults.csv"
+        client = open_vt_session(config.get('API','API_KEY'))
+        AV_ENGINES = config.get('AV','ENGINES').split(',')
+        if args.value:
            check_limit()
-           get_vt_file_obj(client,args.hash)
+           results = get_vt_file_obj(client,args.value,AV_ENGINES)
         else:
            with open(args.infile, 'r') as read_obj:
                csv_reader = csv.DictReader(read_obj)
                for row in csv_reader:
                    check_limit()
-                   get_vt_file_obj(client,row['hash'])
-        csv_writer()
+                   get_vt_file_obj(client,row['hash'],AV_ENGINES)
+        csv_writer(filename)
         close_vt_session(client)
 
 if __name__ == '__main__':
-   sys.exit(main())
+    parser = arg_parse()
+    args = parser.parse_args()
+
+    main(args)
